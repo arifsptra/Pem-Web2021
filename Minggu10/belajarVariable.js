170b1f842f7caa7a3ed7c1817076acb51dc7280a
arifsptra <111202113480@mhs.dinus.ac.id>
2021-12-09 22:18:59 +0700

    Belajar variabel javascript

---
+++ Minggu10/belajarVariable.js
@@ -0,0 +1,14 @@
+// Variabel
+var nama = "Arif Saputra";
+var jumlahPengunjung = 10;
+var aktif = true;
+var link = "https://lynk.id/arif_sptrra";
+
+// Menampilan variabel nama meggunakan alert
+alert("Hai, Nama Saya " + nama);
+
+// Menampilkan variabel ke tampilan website
+document.write("Nama saya: " + nama + "<br>");
+document.write("Pengunjung web saya: " + jumlahPengunjung + "<br>");
+document.write("Status aktif: " + aktif + "<br>");
+document.write("Link sosial media: " + link + "<br>");
--- Minggu10/eksternal-js/dist/kode.dev.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-alert("Hello, ini eksternal JS");
-alert("Bismillah Expert!");
