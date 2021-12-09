66953f7a69c13648f7b756676d37f34363ee4431
arifsptra <111202113480@mhs.dinus.ac.id>
2021-12-09 22:25:02 +0700

    Mengisi ulang nilai variabel dan Menghapus variabel

--- Minggu10/belajarVariable.js
+++ Minggu10/belajarVariable.js
@@ -3,6 +3,13 @@ var nama = "Arif Saputra";
 var jumlahPengunjung = 10;
 var aktif = true;
 var link = "https://lynk.id/arif_sptrra";
+uangSaya = 1000000000000000000000000000000000;
+
+// Isi ulang variabel
+jumlahPengunjung = 1000;
+
+// Hapus Variabel
+delete uangSaya;
 
 // Menampilan variabel nama meggunakan alert
 alert("Hai, Nama Saya " + nama);
@@ -12,3 +19,4 @@ document.write("Nama saya: " + nama + "<br>");
 document.write("Pengunjung web saya: " + jumlahPengunjung + "<br>");
 document.write("Status aktif: " + aktif + "<br>");
 document.write("Link sosial media: " + link + "<br>");
+document.write(uangSaya);
