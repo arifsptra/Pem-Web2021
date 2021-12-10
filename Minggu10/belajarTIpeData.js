d217c44683e747d5d8c08d8d791c8fb3f81b1939
arifsptra <111202113480@mhs.dinus.ac.id>
2021-12-10 08:44:28 +0700

    Belajar Tipe Data Javascript

---
+++ Minggu10/belajarTIpeData.js
@@ -0,0 +1,13 @@
+var nama = "Arif Saputra";
+var namaPanggilan = "Arif";
+var usia = 17;
+var nomorFloat = 10.11;
+var jomblo = false;
+var hobi = ["Bermain Bola", "Membaca Buku", "Traveling"];
+
+document.write(typeof nama);
+document.write(typeof namaPanggilan);
+document.write(typeof usia);
+document.write(typeof nomorFloat);
+document.write(typeof jomblo);
+document.write(typeof hobi);
