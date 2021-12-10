da294b6ba5196f4beb7bd5385c36689bd11e44bd
arifsptra <111202113480@mhs.dinus.ac.id>
2021-12-10 09:55:36 +0700

    Praktikum Aritmatika Javascript

---
+++ Minggu10/aritmatika.js
@@ -0,0 +1,27 @@
+var nilaiA = 10;
+var nilaiB = 11;
+var hasil = 0;
+
+// Penjumlahan
+hasil = nilaiA + nilaiB;
+document.write(`${nilaiA} + ${nilaiB} = ${hasil} <br>`);
+
+// Pengurangan
+hasil = nilaiA - nilaiB;
+document.write(`${nilaiA} - ${nilaiB} = ${hasil} <br>`);
+
+// Perkalian
+hasil = nilaiA * nilaiB;
+document.write(`${nilaiA} * ${nilaiB} = ${hasil} <br>`);
+
+// Pemangkatan
+hasil = nilaiA ** nilaiB;
+document.write(`${nilaiA} ** ${nilaiB} = ${hasil} <br>`);
+
+// Pembagian
+hasil = nilaiA / nilaiB;
+document.write(`${nilaiA} / ${nilaiB} = ${hasil} <br>`);
+
+// Modulus
+hasil = nilaiA % nilaiB;
+document.write(`${nilaiA} % ${nilaiB} = ${hasil} <br>`);
