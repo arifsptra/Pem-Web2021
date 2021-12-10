f5b47810a28c9df3779fc6ac4cfc18a4ae477777
arifsptra <111202113480@mhs.dinus.ac.id>
2021-12-10 10:10:41 +0700

    Praktikum Operator Penugasan Javascript

---
+++ Minggu10/op-penugasan.js
@@ -0,0 +1,21 @@
+document.write("Nilai Pertama = ");
+var score = 100;
+document.write("Nilai " + score + "<br>");
+
+score += 10;
+document.write("Nilai " + score + "<br>");
+
+score -= 11;
+document.write("Nilai " + score + "<br>");
+
+score *= 10;
+document.write("Nilai " + score + "<br>");
+
+score /= 2;
+document.write("Nilai " + score + "<br>");
+
+score **= 2;
+document.write("Nilai " + score + "<br>");
+
+score %= 10;
+document.write("Nilai " + score + "<br>");
