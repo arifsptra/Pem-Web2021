15bba165523776edd5cb544c82a6e6b82a1c073c
arifsptra <111202113480@mhs.dinus.ac.id>
2021-12-10 09:17:58 +0700

    Praktikum Prompt Javascript

---
+++ Minggu10/prompt.js
@@ -0,0 +1,7 @@
+var nama = prompt("Siapa Nama Anda? ", "");
+
+document.write("Selamat datang " + nama + " Di Website Saya!");
+
+var nomorKesukaan = prompt("Berapakah nomor kesukaan anda? ");
+
+document.write("<br>Nomor " + nomorKesukaan);
