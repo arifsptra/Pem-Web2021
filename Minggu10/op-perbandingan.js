503d0bc67cd66e0d0e0401b48ffb9685e75cbabd
arifsptra <111202113480@mhs.dinus.ac.id>
2021-12-11 10:22:04 +0700

    Praktikum Operator Perbandingan Javascript

---
+++ Minggu10/op-perbandingan.js
@@ -0,0 +1,26 @@
+var nilaiA = 10;
+var nilaiB = 11;
+
+// Sama dengan
+var hasil = nilaiA == nilaiB;
+document.write(`${nilaiA} == ${nilaiB} = ${hasil} <br>`);
+
+// Kurang dari
+var hasil = nilaiA < nilaiB;
+document.write(`${nilaiA} < ${nilaiB} = ${hasil} <br>`);
+
+// Lebih dari
+var hasil = nilaiA > nilaiB;
+document.write(`${nilaiA} > ${nilaiB} = ${hasil} <br>`);
+
+// Kurang dari Sama dengan
+var hasil = nilaiA <= nilaiB;
+document.write(`${nilaiA} <= ${nilaiB} = ${hasil} <br>`);
+
+// Lebih dari Sama dengan
+var hasil = nilaiA >= nilaiB;
+document.write(`${nilaiA} >= ${nilaiB} = ${hasil} <br>`);
+
+// Tidak sama dengan
+var hasil = nilaiA != nilaiB;
+document.write(`${nilaiA} != ${nilaiB} = ${hasil} <br>`);
