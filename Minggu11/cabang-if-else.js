6fc9c370b1f1afb6676a7ef3e16c16ec1d50849a
arifsptra <111202113480@mhs.dinus.ac.id>
2021-12-13 10:00:54 +0700

    Praktikum Percabangan if else Javascript

---
+++ Minggu11/cabang-if-else.js
@@ -0,0 +1,15 @@
+// var password = prompt("Password:");
+// if (password == "kopi") {
+//   document.write("<h2>Selamat datang bos!</h2>");
+// } else {
+//   document.write("<p>Password salah, coba lagi!</p>");
+// }
+// document.write("<p>Terima kasih sudah menggunakan aplikasi ini!</p>");
+
+var password = prompt("Masukkan Password: ");
+if (password == "PERSIB") {
+  document.write(password + " JUARA!<br>");
+} else {
+  document.write(password + " TIDAK JUARA!<br>");
+}
+document.write("Hatur Nuhun!");
