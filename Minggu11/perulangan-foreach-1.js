fcaf1d70dc92b61f52fb7680d81f38860f14447a
arifsptra <111202113480@mhs.dinus.ac.id>
2021-12-14 20:16:13 +0700

    Praktikum Perulangan Foreach Javascript

---
+++ Minggu11/perulangan-foreach-1.js
@@ -0,0 +1,9 @@
+var bahasa = ["HTML", "CSS", "JAVA", "SWIFT"];
+for (i = 0; i < bahasa.length; i++) {
+  document.write(i + 1 + ". " + bahasa[i] + "<br>");
+}
+
+// Versi Sederhana
+// for (i in bahasa) {
+//   document.write(i + ". " + bahasa[i] + "<br>");
+// }
---
+++ Minggu11/perulangan-foreach-2.js
@@ -0,0 +1,10 @@
+var hari = ["Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu", "Minggu"];
+
+hari.forEach(function (h) {
+  document.write(hari);
+});
+
+// Atau menggunakan arrow function
+// hari.forEach((h) => {
+//   document.write(hari);
+// });
