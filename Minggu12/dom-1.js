6a8058c5d1048789dd79d401a106acd74bb661c6
arifsptra <111202113480@mhs.dinus.ac.id>
2021-12-21 20:39:13 +0700

    Praktikum DOM getElementById() Javascript

---
+++ Minggu12/dom-1.js
@@ -0,0 +1,16 @@
+// Deklarasi Variabel untuk Akses Element Id Tutorial
+var tutorial = document.getElementById("tutorial");
+// Deklarasi Teks ke Element tutorial
+tutorial.innerHTML = "Tutorial JavaScript";
+// Deklarasi Style CSS ke Element tutorial
+tutorial.style.backgroundColor = "#0088b9";
+tutorial.style.color = "#fff";
+
+var tutorial2 = document.getElementById("tutorial2");
+tutorial2.innerHTML = "Tutorial JavaScript 2.0";
+tutorial2.style.backgroundColor = "gold";
+tutorial2.style.color = "#fff";
+tutorial2.style.padding = "10px";
+
+var tutorial3 = document.getElementById("tutorial3");
+tutorial3.innerHTML = "<button>Believe</button>";
