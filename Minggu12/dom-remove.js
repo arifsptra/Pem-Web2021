b0fc8778e53ec5fbeb3166c3e35b3bb84307f5a7
arifsptra <111202113480@mhs.dinus.ac.id>
2021-12-22 17:37:17 +0700

    Praktikum delete() Javascript

---
+++ Minggu12/dom-remove.js
@@ -0,0 +1,9 @@
+// Hapus Element id rem
+var rem = document.getElementById("rem");
+rem.remove();
+console.log("Element Berhasil Dihapus");
+console.log(rem);
+
+// Hapus Element class rem rem
+var remClass = document.getElementsByClassName("rem")[0];
+remClass.remove();
