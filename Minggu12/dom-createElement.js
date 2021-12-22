826ab37a09874cda52dbf32c6ed3705c74367eff
arifsptra <111202113480@mhs.dinus.ac.id>
2021-12-22 17:25:34 +0700

    Praktikum createElement() Javascript

--- Minggu12/dom-createElement.js
+++ Minggu12/dom-createElement.js
@@ -1,10 +1,11 @@
 // Membuat Element H1
 var judul = document.createElement("h1");
-// Membuat Element P 
-var paragraph = document.createElement("p");
+// Membuat Element P
+var par = document.createElement("p");
 
 // Mengisi Konten Element
 judul.textContent = "Create Element H1";
 document.body.append(judul);
-paragraph.textContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime architecto ipsa ab, doloribus hic dolores sequi quibusdam tenetur velit aliquid temporibus voluptatum nulla, fuga blanditiis eius. In voluptates voluptas, sit dicta, debitis repellat necessitatibus impedit, quos optio consequuntur repudiandae et.";
-document.body.append(paragraph);
+par.textContent =
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime architecto ipsa ab, doloribus hic dolores sequi quibusdam tenetur velit aliquid temporibus voluptatum nulla, fuga blanditiis eius. In voluptates voluptas, sit dicta, debitis repellat necessitatibus impedit, quos optio consequuntur repudiandae et.";
+document.body.append(par);
