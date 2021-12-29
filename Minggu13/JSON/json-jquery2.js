fb7dbabe093e5f199e9f96a43d487f50ccb25527
arifsptra <111202113480@mhs.dinus.ac.id>
2021-12-29 10:25:31 +0700

    JSON JQuery Javascript

--- Minggu13/JSON/json-jquery2.js
+++ Minggu13/JSON/json-jquery2.js
@@ -1,6 +1,23 @@
 $.getJSON("data.json", function (hasil) {
   $.each(hasil, function (i) {
-    document.getElementById("json2").innerHTML +=
-      "NIM: " + hasil.nim + "<br>Nama: " + hasil.nama + "<br>Alamat: " + hasil.alamat + "<br>Jurusan: " + hasil.jurusan + "<br>Makul: <br>" + hasil.makul[i].namaMakul + ", Dosen: " + hasil.makul[i].dosen + "<br>" + "<br><br>";
+    document.getElementById("jsonjquery2").innerHTML +=
+      "NIM: " +
+      hasil.nim +
+      "<br>Nama: " +
+      hasil[i].nama +
+      "<br>Alamat: " +
+      hasil[i].alamat +
+      "<br>Jurusan: " +
+      hasil[i].jurusan +
+      "<br>Makul: <br>" +
+      hasil[i].makul[i].namaMakul +
+      ", Dosen: " +
+      hasil[i].makul[i].dosen +
+      "<br>" +
+      hasil[i].makul[i + 1].namaMakul +
+      ", Dosen: " +
+      hasil[i].makul[i + 1].dosen +
+      "<br>" +
+      "<br><br>";
   });
 });
